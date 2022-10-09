b9c06d4c0e99acab2f5c1249733a2459cb26c058
Gerard jourdain Grégoire Charles <84746155+Charlesjourdain@users.noreply.github.com>
2022-10-09 02:13:35 -0400

    Initialize update-before-closing

---
+++ app/src/ui/installing-update/installing-update.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react'
+
+import { Row } from '../lib/row'
+import {
+  Dialog,
+  DialogContent,
+  OkCancelButtonGroup,
+  DialogFooter,
+} from '../dialog'
+import { updateStore, IUpdateState, UpdateStatus } from '../lib/update-store'
+import { Disposable } from 'event-kit'
+import { Loading } from '../lib/loading'
+import { DialogHeader } from '../dialog/header'
+import { Dispatcher } from '../dispatcher'
+
+interface IInstallingUpdateProps {
+  /**
+   * Event triggered when the dialog is dismissed by the user in the
+   * ways described in the Dialog component's dismissable prop.
+   */
+  readonly onDismissed: () => void
+
+  readonly dispatcher: Dispatcher
+}
+
+/**
+ * A dialog that presents information about the
+ * running application such as name and version.
+ */
+export class InstallingUpdate extends React.Component<IInstallingUpdateProps> {
+  private updateStoreEventHandle: Disposable | null = null
+
+  public constructor(props: IInstallingUpdateProps) {
+    super(props)
+  }
+
+  private onUpdateStateChanged = (updateState: IUpdateState) => {
+    // If the update is not being downloaded (`UpdateStatus.UpdateAvailable`),
+    // i.e. if it's already downloaded or not available, close the window.
+    if (updateState.status !== UpdateStatus.UpdateAvailable) {
+      this.props.dispatcher.quitApp(false)
+    }
+  }
+
+  public componentDidMount() {
+    this.updateStoreEventHandle = updateStore.onDidChange(
+      this.onUpdateStateChanged
+    )
+
+    // Manually update the state to ensure we're in sync with the store
+    this.onUpdateStateChanged(updateStore.state)
+  }
+
+  public componentWillUnmount() {
+    if (this.updateStoreEventHandle) {
+      this.updateStoreEventHandle.dispose()
+      this.updateStoreEventHandle = null
+    }
+  }
+
+  private onQuitAnywayButtonClicked = () => {
+    this.props.dispatcher.quitApp(true)
+  }
+
+  private onCancel = () => {
+    this.props.dispatcher.cancelQuittingApp()
+    this.props.onDismissed()
+  }
+
+  public render() {
+    return (
+      <Dialog
+        id="installing-update"
+        onSubmit={this.props.onDismissed}
+        dismissable={false}
+      >
+        <DialogHeader
+          title={__DARWIN__ ? 'Installing Update…' : 'Installing update…'}
+          dismissable={false}
+        />
+        <DialogContent>
+          <Row className="updating-message">
+            <Loading />
+            <span>
+              Please, do not close GitHub Desktop until the update is completely
+              installed.
+            </span>
+          </Row>
+        </DialogContent>
+        <DialogFooter>
+          <OkCancelButtonGroup
+            okButtonText={__DARWIN__ ? 'Quit Anyway' : 'Quit anyway'}
+            onOkButtonClick={this.onQuitAnywayButtonClicked}
+            onCancelButtonClick={this.onCancel}
+          />
+        </DialogFooter>
+      </Dialog>
+    )
+  }
+}
---
+++ app/src/ui/open-pull-request/open-pull-request-dialog.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react'
+import { IConstrainedValue, IPullRequestState } from '../../lib/app-state'
+import { Branch } from '../../models/branch'
+import { ImageDiffType } from '../../models/diff'
+import { Repository } from '../../models/repository'
+import { DialogFooter, OkCancelButtonGroup, Dialog } from '../dialog'
+import { Dispatcher } from '../dispatcher'
+import { OpenPullRequestDialogHeader } from './open-pull-request-header'
+import { PullRequestFilesChanged } from './pull-request-files-changed'
+
+interface IOpenPullRequestDialogProps {
+  readonly repository: Repository
+  readonly dispatcher: Dispatcher
+
+  /**
+   * The IRepositoryState.pullRequestState
+   */
+  readonly pullRequestState: IPullRequestState
+
+  /**
+   * The currently checked out branch
+   */
+  readonly currentBranch: Branch
+
+  /**
+   * See IBranchesState.defaultBranch
+   */
+  readonly defaultBranch: Branch | null
+
+  /**
+   * See IBranchesState.allBranches
+   */
+  readonly allBranches: ReadonlyArray<Branch>
+
+  /**
+   * See IBranchesState.recentBranches
+   */
+  readonly recentBranches: ReadonlyArray<Branch>
+
+  /** Whether we should display side by side diffs. */
+  readonly showSideBySideDiff: boolean
+
+  /** Whether we should hide whitespace in diff. */
+  readonly hideWhitespaceInDiff: boolean
+
+  /** The type of image diff to display. */
+  readonly imageDiffType: ImageDiffType
+
+  /** Label for selected external editor */
+  readonly externalEditorLabel?: string
+
+  /** Width to use for the files list pane in the files changed view */
+  readonly fileListWidth: IConstrainedValue
+
+  /** If the latest commit of the pull request is not local, this will contain
+   * it's SHA  */
+  readonly nonLocalCommitSHA: string | null
+
+  /** Called to dismiss the dialog */
+  readonly onDismissed: () => void
+}
+
+/** The component for start a pull request. */
+export class OpenPullRequestDialog extends React.Component<IOpenPullRequestDialogProps> {
+  private onCreatePullRequest = () => {
+    this.props.dispatcher.createPullRequest(this.props.repository)
+    // TODO: create pr from dialog pr stat?
+    this.props.dispatcher.recordCreatePullRequest()
+  }
+
+  private renderHeader() {
+    const {
+      currentBranch,
+      pullRequestState,
+      defaultBranch,
+      allBranches,
+      recentBranches,
+    } = this.props
+    const { baseBranch, commitSHAs } = pullRequestState
+    return (
+      <OpenPullRequestDialogHeader
+        baseBranch={baseBranch}
+        currentBranch={currentBranch}
+        defaultBranch={defaultBranch}
+        allBranches={allBranches}
+        recentBranches={recentBranches}
+        commitCount={commitSHAs?.length ?? 0}
+        onDismissed={this.props.onDismissed}
+      />
+    )
+  }
+
+  private renderContent() {
+    return (
+      <div className="open-pull-request-content">
+        {this.renderFilesChanged()}
+      </div>
+    )
+  }
+
+  private renderFilesChanged() {
+    const {
+      dispatcher,
+      externalEditorLabel,
+      hideWhitespaceInDiff,
+      imageDiffType,
+      pullRequestState,
+      repository,
+      fileListWidth,
+      nonLocalCommitSHA,
+    } = this.props
+    const { commitSelection } = pullRequestState
+    const { diff, file, changesetData } = commitSelection
+    const { files } = changesetData
+
+    return (
+      <PullRequestFilesChanged
+        diff={diff}
+        dispatcher={dispatcher}
+        externalEditorLabel={externalEditorLabel}
+        fileListWidth={fileListWidth}
+        files={files}
+        hideWhitespaceInDiff={hideWhitespaceInDiff}
+        imageDiffType={imageDiffType}
+        nonLocalCommitSHA={nonLocalCommitSHA}
+        selectedFile={file}
+        showSideBySideDiff={this.props.showSideBySideDiff}
+        repository={repository}
+      />
+    )
+  }
+
+  private renderFooter() {
+    return (
+      <DialogFooter>
+        <OkCancelButtonGroup
+          okButtonText="Create Pull Request"
+          okButtonTitle="Create pull request on GitHub."
+          cancelButtonText="Cancel"
+        />
+      </DialogFooter>
+    )
+  }
+
+  public render() {
+    return (
+      <Dialog
+        className="open-pull-request"
+        onSubmit={this.onCreatePullRequest}
+        onDismissed={this.props.onDismissed}
+      >
+        {this.renderHeader()}
+        {this.renderContent()}
+        {this.renderFooter()}
+      </Dialog>
+    )
+  }
+}
